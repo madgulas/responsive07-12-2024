36179f773b8b08eea355fac920f598bb8278fd74
Madgula Siddharth <madgul.siddu19@gmail.com>
2024-08-04 21:17:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-gsap.from("#img1",{
+gsap.from("#img1" ,{
     delay: 0.4,
-    opacity: 0,
-    duration: 1,
-    y:60
-}
+    opacity: 100,
+    duration: 2,
+    y:60,
+})
 gsap.from("#img2",{
     delay: 0.4,
     opacity: 0,
     duration: 1,
     x:60
-    
-}
+})
 gsap.from("#img3",{
     delay: 0.4,
     opacity: 0,
     duration: 1,
     y:-60
-}
+})
+
+gsap.from("h1",{
+    y:1000,
+    opacity: 0,
+    duration: 2,
+    delay:0.4
+})
+gsap.from("#part2",{
+    y:1000,
+    opacity: 0,
+    duration: 2,
+    delay:0.4
+    
+})
